d3d9350ace8ef9675850ffa93824442e713c46c4
Dipesh Rishi <dipesh0439@gmail.com>
2020-02-16 12:09:50 +0530

    changes info

---
+++ run.py
@@ -0,0 +1,11 @@
+from wsgiref.simple_server import make_server
+from first import app
+   	
+
+with make_server('',8800,app) as server:
+	print(" serving on port 8800 ....\n Visit http://127.0.0.1:8800")
+
+	server.serve_forever()
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
