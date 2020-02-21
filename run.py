dee838779a17f8a91b97fe590e518c1491ba12f8
dipesh_rishi <36751695+Rishidipesh97@users.noreply.github.com>
2020-02-21 23:48:01 +0530

    Update run.py

--- run.py
+++ run.py
@@ -2,10 +2,10 @@ from wsgiref.simple_server import make_server
 from first import app
    	
 
-with make_server('',8800,app) as server:
-	print(" serving on port 8800 ....\n Visit http://127.0.0.1:8800")
+# with make_server('',8800,app) as server:
+# 	print(" serving on port 8800 ....\n Visit http://127.0.0.1:8800")
 
-	server.serve_forever()
+# 	server.serve_forever()
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
